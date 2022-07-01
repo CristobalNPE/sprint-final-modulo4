1ae7ce670b62c328f1aa4dc6c26613740c1855d8
Cristobal <cristobal.npe@gmail.com>
2022-07-01 00:46:46 -0400

    Implementar validaciones clase Revision

--- src/main/java/org/loshuasostech/modelos/Revision.java
+++ src/main/java/org/loshuasostech/modelos/Revision.java
@@ -1,5 +1,10 @@
 package org.loshuasostech.modelos;
 
+/**
+ * @author Los Huasos TECH
+ * @version 0.1
+ * @since 01-07-2022
+ */
 public class Revision {
   private int idRevision;
   private int idVisita;
@@ -40,24 +45,57 @@ public class Revision {
     return nombre;
   }
 
+  /**
+   * Define el nombre para la revision
+   *
+   * @param nombre Es una cadena entre 10 y 50 caracteres.
+   * @throws IllegalArgumentException si no cumple con la condicion.
+   */
   public void setNombre(String nombre) {
-    this.nombre = nombre;
+    if (nombre.length() < 10 || nombre.length() > 50) {
+      throw new IllegalArgumentException
+              ("El nombre de la revision debe contener entre 10 y 50 caracteres");
+    } else {
+      this.nombre = nombre;
+    }
   }
 
   public String getDetalle() {
     return detalle;
   }
 
+  /**
+   * Define el detalle de la revision
+   *
+   * @param detalle Es una cadena con un maximo de 100 caracteres
+   * @throws IllegalArgumentException si no cumple con la condicion.
+   */
   public void setDetalle(String detalle) {
-    this.detalle = detalle;
+    if (detalle.length() > 100) {
+      throw new IllegalArgumentException
+              ("El detalle de la revision no puede exceder los 100 caracteres");
+    } else {
+      this.detalle = detalle;
+    }
   }
 
   public int getEstado() {
     return estado;
   }
 
+  /**
+   * Define el estado de la revision
+   *
+   * @param estado Corresponde a un entero entre 1 y 3
+   * @throws IllegalArgumentException si no cumple con la condicion.
+   */
   public void setEstado(int estado) {
-    this.estado = estado;
+    if (estado < 1 || estado > 3) {
+      throw new IllegalArgumentException
+              ("Debe indicar un estado valido (entre 1 y 3)");
+    } else {
+      this.estado = estado;
+    }
   }
 
   @Override
