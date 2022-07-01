c8cc05f4a6a05d1ceaa616c6798f095c47164861
Cristobal <cristobal.npe@gmail.com>
2022-06-30 22:51:44 -0400

    Aplicar validaciones necesarias a clase Usuario

--- src/main/java/org/loshuasostech/modelos/Usuario.java
+++ src/main/java/org/loshuasostech/modelos/Usuario.java
@@ -1,18 +1,20 @@
 package org.loshuasostech.modelos;
 
-import java.time.LocalDate;
-
-
+/**
+ * @author Los Huasos TECH
+ * @version 0.1
+ * @since 01-07-2022
+ */
 public class Usuario {
   private String nombre;
-  private LocalDate fechaNacimiento;
+  private String fechaNacimiento;
   private int run;
 
   public Usuario() {
 
   }
 
-  public Usuario(String nombre, LocalDate fechaNacimiento, int run) {
+  public Usuario(String nombre, String fechaNacimiento, int run) {
     this.nombre = nombre;
     this.fechaNacimiento = fechaNacimiento;
     this.run = run;
@@ -22,24 +24,64 @@ public class Usuario {
     return nombre;
   }
 
+  /**
+   * Define el nombre del usuario
+   *
+   * @param nombre Es una cadena entre 10 y 50 caracteres.
+   * @throws IllegalArgumentException si el nombre no cumple con la condicion.
+   */
   public void setNombre(String nombre) {
-    this.nombre = nombre;
+    if (nombre.length() < 10 || nombre.length() > 50) {
+      throw new IllegalArgumentException
+              ("Nombre debe contener al menos 10 caracteres y no mas de 50");
+    } else {
+      this.nombre = nombre;
+    }
+
   }
 
-  public LocalDate getFechaNacimiento() {
-    return fechaNacimiento;
+  public String getFechaNacimiento() {
+
+
+    return this.fechaNacimiento;
   }
 
-  public void setFechaNacimiento(LocalDate fechaNacimiento) {
-    this.fechaNacimiento = fechaNacimiento;
+  /**
+   * Define la fecha de nacimiento del usuario
+   *
+   * @param fechaNacimiento corresponde a una fecha en formato DD/MM/AAAA
+   * @throws IllegalArgumentException si la fecha  no cumple la condicion
+   */
+  public void setFechaNacimiento(String fechaNacimiento) {
+    String formatoFecha =
+            "^([0-2][0-9]|(3)[0-1])(\\/)(((0)[0-9])|((1)[0-2]))(\\/)\\d{4}$";
+    if (!fechaNacimiento.matches(formatoFecha)) {
+      throw new IllegalArgumentException
+              ("Debe ingresar la fecha en formato DD/MM/AAAA");
+    } else {
+      this.fechaNacimiento = fechaNacimiento;
+    }
+
   }
 
   public int getRun() {
     return run;
   }
 
+  /**
+   * Define el RUN para el usuario
+   *
+   * @param run corresponde a un numero menor a 99.999.999
+   * @throws IllegalArgumentException si el numero no cumple la condicion.
+   */
   public void setRun(int run) {
-    this.run = run;
+    if (run >= 99_999_999) {
+      throw new IllegalArgumentException
+              ("El RUN debe ser menor a 99.999.999");
+    } else {
+      this.run = run;
+    }
+
   }
 
   @Override
