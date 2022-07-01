f56097fd4a477b14530c29412489fc4bf934a6a9
Cristobal <cristobal.npe@gmail.com>
2022-07-01 00:15:19 -0400

    Implementar validaciones necesarias clase Accidente

--- src/main/java/org/loshuasostech/modelos/Accidente.java
+++ src/main/java/org/loshuasostech/modelos/Accidente.java
@@ -1,18 +1,20 @@
 package org.loshuasostech.modelos;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
+/**
+ * @author Los Huasos TECH
+ * @version 0.1
+ * @since 01-07-2022
+ */
 public class Accidente {
   private int id;
   private String rut;
-  private LocalDate diaAccidente;
-  private LocalTime hora;
+  private String diaAccidente;
+  private String hora;
   private String lugar;
   private String origen;
   private String consecuencia;
 
-  public Accidente(int id, String rut, LocalDate diaAccidente, LocalTime hora,
+  public Accidente(int id, String rut, String diaAccidente, String hora,
                    String lugar, String origen, String consecuencia) {
 
     this.id = id;
@@ -43,44 +45,103 @@ public class Accidente {
     this.rut = rut;
   }
 
-  public LocalDate getDiaAccidente() {
+  public String getDiaAccidente() {
     return diaAccidente;
   }
 
-  public void setDiaAccidente(LocalDate diaAccidente) {
-    this.diaAccidente = diaAccidente;
+  /**
+   * Define la fecha del accidente
+   *
+   * @param diaAccidente corresponde a una fecha en formato DD/MM/AAAA
+   * @throws IllegalArgumentException si la fecha  no cumple la condicion
+   */
+  public void setDiaAccidente(String diaAccidente) {
+    String formatoFecha =
+            "^([0-2][0-9]|(3)[0-1])(\\/)(((0)[0-9])|((1)[0-2]))(\\/)\\d{4}$";
+    if (!diaAccidente.matches(formatoFecha)) {
+      throw new IllegalArgumentException
+              ("Debe ingresar la fecha del accidente en formato DD/MM/AAAA");
+    } else {
+      this.diaAccidente = diaAccidente;
+    }
   }
 
-  public LocalTime getHora() {
+  public String getHora() {
     return hora;
   }
 
-  public void setHora(LocalTime hora) {
-    this.hora = hora;
+  /**
+   * Define el hora del Accidente
+   *
+   * @param hora Corresponde a una cadena en formato valido HH:MM
+   * @throws IllegalArgumentException si no cumple con la condicion.
+   */
+  public void setHora(String hora) {
+    String formatoHora =
+            "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$";
+    if (!hora.matches(formatoHora)) {
+      throw new IllegalArgumentException
+              ("Debe ingresar la hora del accidente en formato HH:MM");
+    } else {
+      this.hora = hora;
+    }
   }
 
   public String getLugar() {
     return lugar;
   }
 
+  /**
+   * Define el Lugar correspondiente a la capacitacion
+   *
+   * @param lugar Es una cadena entre 10 y 50 caracteres.
+   * @throws IllegalArgumentException si no cumple con la condicion.
+   */
   public void setLugar(String lugar) {
-    this.lugar = lugar;
+    if (lugar.length() < 10 || lugar.length() > 50) {
+      throw new IllegalArgumentException
+              ("El Lugar debe contener al menos 10 caracteres y no mas de 50");
+    } else {
+      this.lugar = lugar;
+    }
   }
 
   public String getOrigen() {
     return origen;
   }
 
+  /**
+   * Define el origen del accidente.
+   *
+   * @param origen Es una cadena con un maximo de 100 caracteres.
+   * @throws IllegalArgumentException si no cumple con la condicion.
+   */
   public void setOrigen(String origen) {
-    this.origen = origen;
+    if (origen.length() > 100) {
+      throw new IllegalArgumentException
+              ("Debe describir el origen en un maximo de 100 caracteres");
+    } else {
+      this.origen = origen;
+    }
   }
 
   public String getConsecuencia() {
     return consecuencia;
   }
 
+  /**
+   * Define las consecuencias del accidente.
+   *
+   * @param consecuencia Es una cadena con un maximo de 100 caracteres.
+   * @throws IllegalArgumentException si no cumple con la condicion.
+   */
   public void setConsecuencia(String consecuencia) {
-    this.consecuencia = consecuencia;
+    if (consecuencia.length() > 100) {
+      throw new IllegalArgumentException
+              ("Debe describir las consecuencias en un maximo de 100 caracteres");
+    } else {
+      this.consecuencia = consecuencia;
+    }
   }
 
   @Override
